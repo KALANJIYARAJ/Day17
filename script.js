657bf749792f144b58c3657ccb2a4bb484b4e195
KALANJIYARAJ <110882243+KALANJIYARAJ@users.noreply.github.com>
2022-09-13 08:45:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let container = document.createElement("div");
+container.setAttribute("class","container");
+
+let row = document.createElement("div");
+row.setAttribute("class","row");
+
+let col = document.createElement("div");
+col.setAttribute("class","col-md");
+
+let h1 = document.createElement("h1");
+
+
+
+setTimeout(()=>{
+    setTimeout(()=>{
+        setTimeout(()=>{
+            setTimeout(()=>{
+                setTimeout(()=>{
+                    setTimeout(()=>{
+                        setTimeout(()=>{
+                            setTimeout(()=>{
+                                setTimeout(()=>{
+                                    setTimeout(()=>{
+                                        setTimeout(()=>{
+        
+                                            h1.innerHTML="Happy Independence Day";
+                                              },1000)
+                                        h1.innerHTML="1";
+                                          },1000)
+                                    h1.innerHTML="2";
+                                      },1000)
+                                h1.innerHTML="3";
+                                  },1000)
+                            h1.innerHTML="4";
+                              },1000)
+                        h1.innerHTML="5";
+                          },1000)
+                    h1.innerHTML="6";
+                      },1000)
+                h1.innerHTML="7";
+                  },1000)
+            h1.innerHTML="8";
+              },1000)
+      h1.innerHTML="9";
+        },1000)
+
+h1.innerHTML="10";
+},0)
+col.append(h1);
+row.append(col);
+container.append(row);
+document.body.append(container);
+
+
+
+
